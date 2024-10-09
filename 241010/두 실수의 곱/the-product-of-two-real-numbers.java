8bf05110a7884ca007ecb4b786cb65fc1edac0cc
Seongwon <134680417+beefplz@users.noreply.github.com>
2024-10-10 02:09:30 +0900

    Title: 두 실수의 곱, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241010/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String a = String.format("%.3f",5.26*8.27);
+        System.out.print(a);
+    }
+}
